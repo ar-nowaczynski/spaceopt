6ed697a04606556dc1f14ce23f4bc83f7835ce80
ar-nowaczynski <ar.nowaczynski@gmail.com>
2020-01-26 20:24:52 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='spaceopt',
       version='0.1.0',
-      description='Search space optimization via predictive modelling',
+      description='Search space optimization via predictive modeling',
       author='Arkadiusz Nowaczynski',
       author_email='ar.nowaczynski@gmail.com',
       url='https://github.com/ar-nowaczynski/spaceopt',
